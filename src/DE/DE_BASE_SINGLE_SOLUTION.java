efccb9170594dfd4be9935c348307f39954086b9
sixtynicooo <sixtynicooo@gmail.com>
2025-01-12 17:09:07 +0100

    Aggiunti algoritmi de

---
+++ src/DE/DE_BASE_SINGLE_SOLUTION.java
@@ -0,0 +1,137 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DE;
+
+import classi_condivise.VariabilGlobali;
+import classi_condivise.random;
+import classi_condivise.utility;
+import javax.crypto.spec.DESKeySpec;
+
+/**
+ *
+ * @author sixty
+ */
+public class DE_BASE_SINGLE_SOLUTION {
+
+    // variabili globali
+    static VariabilGlobali variabilGlobali;
+    static DE.INDIVIDUO_DE_SINGLE_SOLUTION[] listaIndividui;    // lista individui
+    static DE.INDIVIDUO_DE_SINGLE_SOLUTION globalFitnessMIgliore;    // lista individui
+
+    static utility utilita = new utility();
+    static random rand = new random();
+
+    public DE_BASE_SINGLE_SOLUTION(VariabilGlobali variabilGlobali) {
+        DE_BASE_SINGLE_SOLUTION.variabilGlobali = new VariabilGlobali();
+        DE_BASE_SINGLE_SOLUTION.variabilGlobali = variabilGlobali;
+    }
+
+    public void run() {
+        inizializza();
+        generazioni();
+    }
+
+    private void inizializza() {
+        // Creazione dell'array di individui
+        listaIndividui = new DE.INDIVIDUO_DE_SINGLE_SOLUTION[variabilGlobali.NUM_INDIVIDUO];
+        globalFitnessMIgliore = new DE.INDIVIDUO_DE_SINGLE_SOLUTION(variabilGlobali); // Crea un nuovo individuo
+        // Inizializzazione degli individui
+        for (int i = 0; i < variabilGlobali.NUM_INDIVIDUO; i++) {
+            listaIndividui[i] = new DE.INDIVIDUO_DE_SINGLE_SOLUTION(variabilGlobali);
+            listaIndividui[i].calcoloFitness();
+            //listaIndividui[i].stampa();
+        }
+        globalFitnessMIgliore.calcoloFitness();
+
+        for (int i = 0; i < variabilGlobali.NUM_INDIVIDUO; i++) {
+            listaIndividui[i].aggiornaFitnessGlobale(globalFitnessMIgliore);
+        }
+
+    }
+
+    private void generazioni() {
+
+        boolean migliorato = false;
+        int indiceMovimenti = 0;
+        int[] indiciIndividui = new int[variabilGlobali.NUM_INDIVIDUO];
+        for (int i = 0; i < variabilGlobali.NUM_INDIVIDUO; i++) {
+            indiciIndividui[i] = i;
+        }
+        utilita.mescolaArray(indiciIndividui);
+
+        DE.INDIVIDUO_DE_SINGLE_SOLUTION mutante;    // lista individui
+        mutante = new DE.INDIVIDUO_DE_SINGLE_SOLUTION(variabilGlobali);
+        System.out.println("DE.DE_BASE_SINGLE_SOLUTION.generazioni()");
+       
+
+        for (int iter = 0; iter < variabilGlobali.ITERAZIONI; iter++) {
+
+            for (int individuo = 0; individuo + 3 < variabilGlobali.NUM_INDIVIDUO; individuo += 4) {
+
+                int genitorePrimario = indiciIndividui[individuo];
+                int mutante1 = indiciIndividui[individuo+ 1] ;
+                int mutante2 = indiciIndividui[individuo+ 2] ;
+                int mutante3 = indiciIndividui[individuo+ 3] ;
+
+                // operatore mutazione
+                mutazione(mutante1, mutante2, mutante3, mutante);
+                // operatore crossower
+                //crossower(mutante, genitorePrimario);
+                // operatore selezione
+                if (selezione(mutante, genitorePrimario)) {
+                    indiceMovimenti = 0;
+                    migliorato = true;
+                }
+
+            }
+            // aggiorno migliore soluzione
+            if (indiceMovimenti > variabilGlobali.STAZIONARIETA) {
+                System.out.println("Uscito per stazionarieta " + iter);
+                break;
+            }
+            // semigliorato resetto
+
+            indiceMovimenti++;
+            utilita.mescolaArray(indiciIndividui);
+        }
+        globalFitnessMIgliore.stampa();
+    }
+
+    private void mutazione(int mutante1, int mutante2, int mutante3, INDIVIDUO_DE_SINGLE_SOLUTION mutante) {
+        for (int d = 0; d < variabilGlobali.DIM_ARR_DOUBLE; d++) {
+            mutante.variabili_Individuo.arrDouble[d] = listaIndividui[mutante1].variabili_Individuo.arrDouble[d]
+                    + variabilGlobali.DE_q * (listaIndividui[mutante2].variabili_Individuo.arrDouble[d]
+                    - listaIndividui[mutante3].variabili_Individuo.arrDouble[d]);
+            mutante.variabili_Individuo.arrDouble[d] = utilita.verificaIntervalloDouble(mutante.variabili_Individuo.arrDouble[d], variabilGlobali.ARR_DOUBLE_MIN[d], variabilGlobali.ARR_DOUBLE_MAX[d]);
+
+        }
+    }
+
+    private void crossower(INDIVIDUO_DE_SINGLE_SOLUTION mutante, int genitorePrimario) {
+        for (int d = 0; d < variabilGlobali.DIM_ARR_DOUBLE; d++) {
+            // se il numero casuale è < lascio la variabile della mutazione
+            if (rand.generateRandomDouble(0, 1) > variabilGlobali.DE_PROBABILITA_CROSSOWER) {
+                mutante.variabili_Individuo.arrDouble[d] = listaIndividui[genitorePrimario].variabili_Individuo.arrDouble[d];
+            }
+
+        }
+    }
+
+    private boolean selezione(INDIVIDUO_DE_SINGLE_SOLUTION mutante, int genitorePrimario) {
+        boolean migliorato = false;
+        // Calcola la fitness del mutante
+        mutante.calcoloFitness();
+        // Confronta la fitness del mutante con il genitore
+        if ((variabilGlobali.problemaMassimizzareMinimizzare && mutante.getFitness() > listaIndividui[genitorePrimario].getFitness())
+                || (!variabilGlobali.problemaMassimizzareMinimizzare && mutante.getFitness() < listaIndividui[genitorePrimario].getFitness())) {
+            // Sostituisci il genitore con il mutante se la fitness è migliore
+            listaIndividui[genitorePrimario].variabili_Individuo = mutante.copiare();
+            migliorato = listaIndividui[genitorePrimario].aggiornaFitnessGlobale(globalFitnessMIgliore);
+        }
+
+        return migliorato;
+    }
+
+}
---
+++ src/DE/INDIVIDUO_DE_SINGLE_SOLUTION.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DE;
+
+import classi_condivise.Fitness;
+import classi_condivise.VariabilGlobali;
+import classi_condivise.Variabili_Fitness_Migliori.Variabili_Individuo;
+import classi_condivise.random;
+import classi_condivise.utility;
+
+/**
+ *
+ * @author sixty
+ */
+public class INDIVIDUO_DE_SINGLE_SOLUTION implements utility.FitnessEntity {
+
+    static VariabilGlobali variabilGlobali;
+    Variabili_Individuo variabili_Individuo;
+
+    static random rand = new random();
+    static Fitness fitnessClass = new Fitness();
+    static utility utilita = new utility();
+
+    public INDIVIDUO_DE_SINGLE_SOLUTION(VariabilGlobali variabilGlobali) {
+        INDIVIDUO_DE_SINGLE_SOLUTION.variabilGlobali = new VariabilGlobali();
+        INDIVIDUO_DE_SINGLE_SOLUTION.variabilGlobali = variabilGlobali;
+
+        this.variabili_Individuo = new Variabili_Individuo(variabilGlobali);
+        // inizializzo ARR_DOUBLE
+        for (int i = 0; i < variabilGlobali.DIM_ARR_DOUBLE; i++) {
+            this.variabili_Individuo.arrDouble[i] = rand.generateRandomDouble(variabilGlobali.ARR_DOUBLE_MIN[i], variabilGlobali.ARR_DOUBLE_MAX[i]);
+        }
+    }
+
+    public void calcoloFitness() {
+        // Calcolo del fitness attuale
+        this.variabili_Individuo.fitness = fitnessClass.fitness(this.variabili_Individuo.arrDouble);
+
+        // Verifica miglioramento locale in base al tipo di problema
+        utilita.verificaMiglioramentoLocale(this.variabili_Individuo, variabilGlobali.problemaMassimizzareMinimizzare);
+    }
+
+    public boolean aggiornaFitnessGlobale(INDIVIDUO_DE_SINGLE_SOLUTION globalFitnessMIgliore) {
+        return utilita.aggiornaFitnessGlobale(globalFitnessMIgliore, this.variabili_Individuo, variabilGlobali.problemaMassimizzareMinimizzare);
+
+    }
+
+    @Override
+    public double getFitness() {
+        return this.variabili_Individuo.fitness;
+    }
+
+    @Override
+    public void setFitness(double fitness) {
+        this.variabili_Individuo.fitness = fitness;
+    }
+
+    @Override
+    public void setArrDouble(double[] ARR_DOUBLE_POS) {
+        this.variabili_Individuo.arrDouble = ARR_DOUBLE_POS;
+    }
+
+    @Override
+    public void stampa() {
+        System.out.println(" fitness " + this.variabili_Individuo.fitness + " x= " + this.variabili_Individuo.arrDouble[0] + " y= " + this.variabili_Individuo.arrDouble[1]);
+    }
+    
+    // Metodo clone standard (shallow copy)
+    @Override
+    public INDIVIDUO_DE_SINGLE_SOLUTION clone() {
+        INDIVIDUO_DE_SINGLE_SOLUTION cloned = new INDIVIDUO_DE_SINGLE_SOLUTION(variabilGlobali);
+        cloned.variabili_Individuo.arrDouble=variabili_Individuo.arrDouble.clone();
+        
+        return cloned;
+    }
+
+     // Metodo per copiare i dati di variabili_Individuo
+    public Variabili_Individuo copiare() {
+        return variabili_Individuo.copiaVariabiliIndividuo(variabilGlobali);
+    }
+
+}
